c5c6f5913918258e4105daa8f7d3b198eb38b5a4
farsia <Farshid.Saeidi@hotmail.com>
2019-12-21 17:16:58 +0330

    Update Bank.cs

--- Faza.Treasury.Common/Entities/Bank.cs
+++ Faza.Treasury.Common/Entities/Bank.cs
@@ -2,6 +2,7 @@ using Bitspco.Framework.Data.Entities;
 using Bitspco.Framework.Data.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,11 @@ using System.Threading.Tasks;
 namespace Faza.Treasury.Common.Entities
 {
     [Table("Bank", Schema = "Accounting")]
-    public class Bank : Entity
+    public class Bank : Entity, IHasCreationTime
     {
-
+        [StringLength(100), Required]
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
